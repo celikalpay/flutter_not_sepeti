bab53ec1c8a95b16d5c141ae360325883c689ad5
alpaycelik <alpay.celik@bil.omu.edu.tr>
2021-05-14 11:05:25 +0300

    düzenleme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'utils/database_helper.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,9 +9,103 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: Text(""),
+      home: NotListesi(),
+    );
+  }
+}
+
+class NotListesi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text("Not Sepeti"),
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (context) {
+                    return SimpleDialog(
+                      title: Text(
+                        "Kategori Ekle",
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                      children: [
+                        Form(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              validator: (girilenKategoriAdi) {
+                                if (girilenKategoriAdi.length < 3) {
+                                  return "En az 3 karakter giriniz.";
+                                } else {
+                                  return null;
+                                }
+                              },
+                              decoration: InputDecoration(
+                                labelText: "Kategori Adı",
+                                border: OutlineInputBorder(),
+                              ),
+                            ),
+                          ),
+                        ),
+                        ButtonBar(
+                          children: [
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text(
+                                "Vazgeç",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.orangeAccent,
+                              ),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {},
+                              child: Text(
+                                "Kaydet",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.redAccent,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    );
+                  });
+            },
+            tooltip: "Kategori Ekle",
+            child: Icon(Icons.add_circle),
+            mini: true,
+          ),
+          FloatingActionButton(
+            onPressed: () {},
+            tooltip: "Not Ekle",
+            child: Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Container(),
     );
   }
 }
